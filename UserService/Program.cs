1c3ce4a79fc72a22c9b5776cfb7b2128f5416c90
Art'm <kasko.artem45@gmail.com>
2026-07-05 22:21:25 +0300

    DI Configuration

--- UserService/Program.cs
+++ UserService/Program.cs
@@ -1,6 +1,24 @@
+using Microservices.Database;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<UserDbContext>(options => 
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DbConnection")));
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// app.UseHttpsRedirection();
+// app.UseAuthorization();
+app.MapControllers();
 
 app.Run();
