177592379c7465bf75d645431509039c18989f60
Art'm <kasko.artem45@gmail.com>
2026-07-05 22:20:21 +0300

    Add UserDbContext

---
+++ UserService/Database/UserDbContext.cs
@@ -0,0 +1,27 @@
+using Microservices.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Microservices.Database;
+
+public class UserDbContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+
+    public UserDbContext(DbContextOptions<UserDbContext> options) : base(options)
+    {
+        
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        var userBuilder = modelBuilder.Entity<User>();
+        
+        userBuilder.Property(x => x.Id).IsRequired();
+        userBuilder.Property(x => x.Name).IsRequired();
+        userBuilder.Property(x => x.Email).IsRequired();
+        
+        userBuilder.HasKey(x => x.Id);
+        
+        
+    }
+}
